70ae5ad8105b232dbf58a4fa11abcf4008611156
Xiomara <xio0809@gmail.com>
2026-01-29 09:25:02 -0500

    Subiendo estructura inicial del proyecto

---
+++ CalzadosMorales/src/main/java/com/example/demo/CalzadosMoralesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalzadosMoralesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalzadosMoralesApplication.class, args);
+	}
+
+}
