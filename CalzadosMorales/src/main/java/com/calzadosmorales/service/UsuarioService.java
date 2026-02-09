b3e8653ea1316476d79ccece627b6d9c84add7c0
Xiomara <xio0809@gmail.com>
2026-02-08 22:58:25 -0500

    Usuarios listo

---
+++ CalzadosMorales/src/main/java/com/calzadosmorales/service/UsuarioService.java
@@ -0,0 +1,40 @@
+package com.calzadosmorales.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.calzadosmorales.entity.Rol;
+import com.calzadosmorales.entity.Usuario;
+import com.calzadosmorales.repository.RolRepository;
+import com.calzadosmorales.repository.UsuarioRepository;
+
+
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepo;
+    
+    @Autowired
+    private RolRepository rolRepo;
+
+
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepo.findAll();
+    }
+
+    public void guardarUsuario(Usuario usuario) {
+        usuarioRepo.save(usuario);
+    }
+
+    public Usuario buscarUsuarioPorId(Integer id) {
+        return usuarioRepo.findById(id).orElse(null);
+    }
+    
+
+    public List<Rol> listarRoles() {
+        return rolRepo.findAll();
+    }
+}
